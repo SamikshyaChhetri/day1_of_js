27b621c17e691fb0432d851524fd2f746ed6f361
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-04-19 20:45:43 +0545

    day3

--- day3.js
+++ day3.js
@@ -1,8 +1,15 @@
 const ipp=document.getElementById('ip');
 const ch=document.getElementById('check');
-ch.addEventListener('click',function(){
+ch.addEventListener('click',()=>{
     const element=ipp.value;
+    if(element!==''){
     const display=document.getElementById('disp');
-    display.innerHTML+=element+"<br>";
+    display.innerHTML+=`<div class="item">${element}</div>`;
+    ipp.value='';
+    }
+    else{
+        alert("Type something.Empty input cannot be listed");
+    }
 })
+
 
