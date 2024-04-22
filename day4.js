a41020541003edc71e3fcca0bc411219446beda5
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-04-23 01:08:17 +0545

    day4

--- day4.js
+++ day4.js
@@ -27,71 +27,58 @@
 // console.log(element.name);
 // console.log(`${element.name} is ${element.age} years old`);
 
-const ipp = document.getElementById('ip');
-const ch = document.getElementById('check');
+const ipp = document.getElementById("ip");
+const ch = document.getElementById("check");
 let arr = [];
-let deleteditems=[];
-ch.addEventListener('click', (e) => {
-    console.log(e.target.getAttribute('id'));
-    const element = ipp.value;
-    if(element!==''){
+let deleteditems = [];
+ch.addEventListener("click", (e) => {
+  console.log(e.target.getAttribute("id"));
+  const element = ipp.value;
+  if (element !== "") {
     arr.push(element);
-    ipp.value = '';
+    ipp.value = "";
     display();
-    }
-    else {
-        alert("Empty string cannot be listed");
-    }
+    eventlisteneradder();
+  } else {
+    alert("Empty string cannot be listed");
+  }
 });
 function display() {
-    // deleted.innerHTML="";
-    const display_arr = document.getElementById('disp');
-    display_arr.innerHTML="";
-    arr.forEach((a,index)=>{
-        display_arr.innerHTML+=`<div class="main">
+  // deleted.innerHTML="";
+  const display_arr = document.getElementById("disp");
+  display_arr.innerHTML = "";
+  arr.forEach((a, index) => {
+    display_arr.innerHTML += `<div class="main">
         <div class="dis">${a}
         <button data-index=${index} type="check" class="delete">Delete</button></div></div>`;
-
-    })
+  });
 }
-const deleteitem=document.querySelectorAll('.delete');
-deleteitem.forEach((a)=>{
-a.addEventListener('click',()=>{
-    const index=a.getAttribute('data-index');
-    deleteditems.push(arr[index]);
-    arr.splice(index,1);
-    display();
-})
-})
-// a.addEventListener('onclick',()=>{
-//     const index=a.getAttribute('data-index');
-//     deleteditems.push(arr[index]);
-//     arr.splice(index,1);
-//     display();
-// })
-const dlt=document.getElementById("deleted_item");
-dlt.addEventListener('click',()=>{;
-    const deleted=document.getElementById("disp");
-    if(deleteditems.length!==0){
-    deleteditems.forEach((a,index)=>{
-        deleted.innerHTML="";
-        deleted.innerHTML+=`<div class="diss">${a}`;
+const eventlisteneradder = () => {
+  const deleteitem = document.querySelectorAll(".delete");
+  deleteitem.forEach((a) => {
+    a.addEventListener("click", () => {
+      const index = a.getAttribute("data-index");
+      deleteditems.push(arr[index]);
+      arr.splice(index, 1);
+      display();
     });
-}
-else{
-        alert("No items have been deleted");
-    }
+  });
+};
+const dlt = document.getElementById("deleted_item");
+dlt.addEventListener("click", () => {
+  const deleted = document.getElementById("disp");
+  if (deleteditems.length !== 0) {
+    deleteditems.forEach((a, index) => {
+      deleted.innerHTML = "";
+      deleted.innerHTML += `<div class="diss">${a}`;
+    });
+  } else {
+    alert("No items have been deleted");
+  }
 });
-const pend=document.getElementById('pending_item');
-pend.addEventListener('click',()=>{
-    const pending=document.getElementById("disp");
-    pending.innerHTML="";
-    display();
-})
-
-
-
-
-
-
-
+const pend = document.getElementById("pending_item");
+pend.addEventListener("click", () => {
+  const pending = document.getElementById("disp");
+  pending.innerHTML = "";
+  display();
+});
