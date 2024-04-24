cd4c4d732136046e3d284d598f2072074986a361
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-04-25 00:40:21 +0545

    digital clock

---
+++ day6.js
@@ -0,0 +1,26 @@
+// const a=document.getElementById("clock")
+// setInterval(() => {
+//     a.innerHTML=new Date();
+// }, 1000);
+
+const monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+setInterval(() => {
+    const hr = document.getElementById("hrs");
+    const min = document.getElementById("minute");
+    const sec = document.getElementById("second");
+    const mon = document.getElementById("month");
+    const dayy = document.getElementById("day");
+    const datee = document.getElementById("dat");
+    const date = new Date();
+    hr.innerHTML = date.getHours() + " : ";
+    min.innerHTML = date.getMinutes() + " : ";
+    sec.innerHTML = date.getSeconds();
+    mon.innerHTML=monthName[date.getMonth()]+",";
+    dayy.innerHTML =dayName[date.getDay()];
+    datee.innerHTML = date.getDate();
+}, 1);
+
+
+
