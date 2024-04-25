35854f02bd0ef1320317dda426eed04da6ab7ca7
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-04-25 21:44:38 +0545

    digital clock

--- day6.js
+++ day6.js
@@ -1,6 +1,27 @@
-const monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-const dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-setInterval(() => {
+const monthName = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const dayName = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  setInterval(() => {
     const hr = document.getElementById("hrs");
     const min = document.getElementById("minute");
     const sec = document.getElementById("second");
@@ -8,13 +29,37 @@ setInterval(() => {
     const dayy = document.getElementById("day");
     const datee = document.getElementById("dat");
     const date = new Date();
-    hr.innerHTML=date.getHours()+" : ";
-    min.innerHTML = date.getMinutes() + " : ";
-    sec.innerHTML = date.getSeconds();
-    mon.innerHTML=monthName[date.getMonth()]+",";
-    dayy.innerHTML =dayName[date.getDay()];
+    if (date.getHours() > 12) {
+      if (date.getHours() < 22) {
+        hr.innerHTML = "0" + (date.getHours() - 12) + ":";
+      } else {
+        hr.innerHTML = date.getHours() - 12 + ":";
+      }
+    } else {
+      if (date.getHours() < 10) {
+        hr.innerHTML = "0" + date.getHours() + ":";
+      } else {
+        hr.innerHTML = date.getHours() + ":";
+      }
+    }
+    if (date.getMinutes() < 10) {
+      min.innerHTML = "0" + date.getMinutes() + " : ";
+    } else {
+      min.innerHTML = date.getMinutes() + ":";
+    }
+    if (date.getSeconds() < 10) {
+      sec.innerHTML = "0" + date.getSeconds();
+    } else {
+      sec.innerHTML = date.getSeconds();
+    }
+    const hehe = document.getElementById("state");
+    if (date.getHours() < 12) {
+      hehe.innerHTML = "AM";
+    } else {
+      hehe.innerHTML = "PM";
+    }
+    mon.innerHTML = monthName[date.getMonth()] + ",";
+    dayy.innerHTML = dayName[date.getDay()];
     datee.innerHTML = date.getDate();
-}, 1);
-
-
-
+  }, 1);
+  
