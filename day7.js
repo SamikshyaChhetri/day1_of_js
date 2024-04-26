c3acb212a966b1392caab27aacb833f6ffbe7ebd
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-04-26 21:58:53 +0545

    move anywhere

--- day7.js
+++ day7.js
@@ -1,30 +1,35 @@
+console.log(window.innerHeight);
 const a = document.getElementById("child");
 document.addEventListener("keydown", (e) => {
   if (e.key === "ArrowRight") {
     let b = parseInt(a.style.left);
-    if (b < window.innerWidth - 300) {
-      a.style.left = b + 1 + "px";
+    if (b <= window.innerWidth - 175) {
+      a.style.left = b + 10 + "px";
+    } else {
+      alert("Boundary reached");
     }
   } else if (e.key === "ArrowLeft") {
     let b = parseInt(a.style.left);
-    if (b != "1") {
-      a.style.left = b - 1 + "px";
+    if (b > "1") {
+      a.style.left = b - 10 + "px";
     } else {
-      alert("boundary reached");
+      alert("Boundary reached");
     }
   } else if (e.key === "ArrowUp") {
     let b = parseInt(a.style.top);
-    if (b != "1") {
-      a.style.top = b - 1 + "px";
+    if (b > "1") {
+      a.style.top = b - 10 + "px";
     } else {
       alert("Boundary reached");
     }
   } else if (e.key === "ArrowDown") {
     let b = parseInt(a.style.top);
-    if (b < window.innerHeight - 150) {
-      a.style.top = b + 1 + "px";
+    if (b <= window.innerHeight - 100) {
+      a.style.top = b + 10 + "px";
     } else {
-      alert("Invalid key");
+      alert("Boundary reached");
     }
+  } else {
+    alert("Invalid key");
   }
 });
