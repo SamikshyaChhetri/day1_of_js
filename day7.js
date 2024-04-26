4ea3459a51938ac48bda8418f742459f80f59a8e
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-04-26 22:27:18 +0545

    move anywhere

--- day7.js
+++ day7.js
@@ -3,7 +3,7 @@ const a = document.getElementById("child");
 document.addEventListener("keydown", (e) => {
   if (e.key === "ArrowRight") {
     let b = parseInt(a.style.left);
-    if (b <= window.innerWidth - 175) {
+    if (b <= 1420 - 175) {
       a.style.left = b + 10 + "px";
     } else {
       alert("Boundary reached");
@@ -24,7 +24,7 @@ document.addEventListener("keydown", (e) => {
     }
   } else if (e.key === "ArrowDown") {
     let b = parseInt(a.style.top);
-    if (b <= window.innerHeight - 100) {
+    if (b <= 680 - 100) {
       a.style.top = b + 10 + "px";
     } else {
       alert("Boundary reached");
