c15558a96a4fd246c7e75bceca8343a1cf650e4d
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-04-26 13:45:29 +0545

    move the element

---
+++ day7.js
@@ -0,0 +1,15 @@
+// document.addEventListener("keydown",(e)=>{
+//     console.log(e.key);
+// })
+const a = document.getElementById("child");
+console.log(a.style.backgroundColor);
+// document.addEventListener("keydown", (e) => {
+//   if (e.key === "ArrowRight") {
+//     a.style.left += "1px";
+//   } else if (e.key === "ArrorLeft") {
+//   } else if (e.key === "ArrowTop") {
+//   } else if (e.key === "ArrowBottom") {
+//   } else {
+//     alert("Invalid key");
+//   }
+// });
