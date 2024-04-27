7db3a255089d291f35a57f531fb723d189d4fb18
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-04-28 01:38:47 +0545

    You can't say no

--- day9.js
+++ day9.js
@@ -1,7 +1,7 @@
 const element = document.getElementById("no");
 element.addEventListener("mouseover", (e) => {
-  const randomx = Math.floor(Math.random() * window.innerWidth);
-  const randomy = Math.floor(Math.random() * window.innerHeight);
+  const randomx = Math.floor(Math.random() * (window.innerWidth - 50));
+  const randomy = Math.floor(Math.random() * (window.innerHeight - 50));
   element.style.left = randomx + "px";
   element.style.top = randomy + "px";
 });
