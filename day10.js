741a9b86687756cdf097086d95ae418cae813bed
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-03 18:52:22 +0545

    Calculator

--- day10.js
+++ day10.js
@@ -1,45 +1,57 @@
 let input_str = "";
+let running = false;
 const displa = document.getElementById("display");
 const element = document.querySelectorAll(".itm");
 element.forEach((item) => {
   item.addEventListener("click", (e) => {
-    input_str += e.target.getAttribute("data-value");
-    displa.value = input_str;
+    if (running) {
+      input_str += e.target.getAttribute("data-value");
+      displa.value = input_str;
+    }
   });
 });
 
 const res = document.querySelector(".eql");
 res.addEventListener("click", calculate);
-function calculate() {
-  try {
-    const result = eval(input_str);
-    displa.value = result;
-    input_str = result;
-  } catch (error) {
-    displa.value = "Syntax Error!";
-    displa.style.color = "red";
-    displa.style.fontSize = "20px";
-    input_str = "";
+if (running) {
+  function calculate() {
+    try {
+      const result = eval(input_str);
+      displa.value = result;
+      input_str = result;
+    } catch (error) {
+      displa.value = "Syntax Error!👊🏼";
+      displa.style.color = "red";
+      displa.style.fontSize = "20px";
+      input_str = "";
+    }
   }
 }
 const ac = document.querySelector(".Aclear");
 ac.addEventListener("click", aclear);
-function aclear() {
-  input_str = "";
-  displa.value = input_str;
-  displa.placeholder = "0";
+if (running) {
+  function aclear() {
+    input_str = "";
+    displa.value = input_str;
+    displa.placeholder = "0";
+  }
 }
 const c = document.querySelector(".clear");
 c.addEventListener("click", () => {
-  input_str = input_str.slice(0, -1);
-  displa.value = input_str;
+  if (running) {
+    input_str = input_str.slice(0, -1);
+    displa.value = input_str;
+  }
 });
 const onn = document.querySelector(".on");
 onn.addEventListener("click", () => {
   if (onn.innerHTML == "ON") {
     onn.innerHTML = "OFF";
+    running = true;
+    displa.innerHTML = "";
   } else {
     onn.innerHTML = "ON";
+    running = false;
     displa.placeholder = "Press ON to start";
   }
 });
