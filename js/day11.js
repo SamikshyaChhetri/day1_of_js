22f9d8d59246c699491dc0e45bbcb69eb5d53ade
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-08 18:06:15 +0545

    modified decryption

--- js/day11.js
+++ js/day11.js
@@ -94,12 +94,12 @@ const letters = [
   "\\",
 ];
 const encrpt = document.getElementById("encryption");
-encrpt.addEventListener("click", myfunction);
+encrpt.addEventListener("click", encryption_function);
 
-function myfunction() {
+function encryption_function() {
   const input = document.getElementById("inputWord");
   const key = document.getElementById("keyy");
-  const key_val = key.value;
+  const key_val = parseInt(key.value);
   const store = input.value;
   const split_store = store.split("");
   const new_array = split_store.map((i) => {
@@ -108,10 +108,23 @@ function myfunction() {
   });
   const display_word = document.getElementById("display");
   display_word.innerHTML = new_array.join("");
+  const hehe = new_array.join("");
 }
 
-// const decrypt = document.getElementById("decryption");
-// decrypt.addEventListener("click", my_function);
-// function my_function(){
-//     const
-// }
+const decrypt = document.getElementById("decryption");
+decrypt.addEventListener("click", decryption_function);
+function decryption_function() {
+  const input = document.getElementById("inputWord").value;
+  const key = document.getElementById("keyy");
+  const key_val = parseInt(key.value);
+
+  const split_store = input.split("");
+  const new_dec = split_store.map((i) => {
+    const word = (letters.indexOf(i) - key_val) % 92;
+    return letters[word];
+  });
+  const display_word = document.getElementById("display");
+
+  display_word.innerHTML = new_dec.join("");
+  console.log(key_val);
+}
