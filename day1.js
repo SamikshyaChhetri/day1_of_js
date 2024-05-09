2b1b795936bb55c5d5eeb09f37faccadd3372a47
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-09 22:28:44 +0545

    encryption function modiefied

--- day1.js
+++ day1.js
@@ -1,32 +1,30 @@
 const employees = [
-    { name: 'John', salary: 50000 },
-    { name: 'Alice', salary: 60000 },
-    { name: 'Bob', salary: 55000 },
-    { name: 'Emily', salary: 52000 },
-    { name: 'Michael', salary: 58000 },
-    { name: 'Sophia', salary: 54000 },
-    { name: 'William', salary: 56000 },
-    { name: 'Emma', salary: 59000 },
-    { name: 'James', salary: 57000 },
-    { name: 'Olivia', salary: 61000 }
- ];
-
- employees.forEach((a)=>{
-     console.log(`${a.name} earns ${a.salary}`);
- })
+  { name: "John", salary: 50000 },
+  { name: "Alice", salary: 60000 },
+  { name: "Bob", salary: 55000 },
+  { name: "Emily", salary: 52000 },
+  { name: "Michael", salary: 58000 },
+  { name: "Sophia", salary: 54000 },
+  { name: "William", salary: 56000 },
+  { name: "Emma", salary: 59000 },
+  { name: "James", salary: 57000 },
+  { name: "Olivia", salary: 61000 },
+];
+employees.forEach((a) => {
+  console.log(`${a.name} earns ${a.salary}`);
+});
 //  for (var i=0; i<employees.length; i++){
-    //  console.log(employees[i].name+" " + "earns"+ " "+ employees[i].salary)
-    // document.write("NAME : " + employees[i].name+" "+ "Salary :"+employees[i].salary+"<br>");
-    // var result=employees[i];
-    // document.getElementById(sam).innerHTML=result;
+//  console.log(employees[i].name+" " + "earns"+ " "+ employees[i].salary)
+// document.write("NAME : " + employees[i].name+" "+ "Salary :"+employees[i].salary+"<br>");
+// var result=employees[i];
+// document.getElementById(sam).innerHTML=result;
 //  }
 
 //  const myarr=['2','5','9','5'];
 //  myarr.forEach(function(value){
 //  console.log(value)});
 //  function print(value){
-    //  console.log(value)
-
+//  console.log(value)
 
 //  const print=(a)=>{
 //       console.log("hello");
