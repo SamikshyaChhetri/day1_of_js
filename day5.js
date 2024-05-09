5a924c257d900f13311fef7c08d97a49a5aaf497
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-09 22:24:50 +0545

    removed unnecessary images

--- day5.js
+++ day5.js
@@ -1,17 +1,14 @@
-const a=new Date();
-document.getElementById('date').innerHTML=`The current date and time is:  `+a;
+const a = new Date();
+document.getElementById("date").innerHTML =
+  `The current date and time is:  ` + a;
 
-
-const b=document.getElementById("set_interval");
+const b = document.getElementById("set_interval");
 setInterval(() => {
-    b.innerHTML="Hi i am samikshya"
+  b.innerHTML = "Hi i am samikshya";
 }, 2000);
-
-
-const myTimeout = setTimeout(()=>{
-  document.getElementById("settime").innerHTML = "hi samikshya...!"
+const myTimeout = setTimeout(() => {
+  document.getElementById("settime").innerHTML = "hi samikshya...!";
 }, 5000);
-
 // function myGreeting() {
 //   document.getElementById("settime").innerHTML = "hi samikshya...!"
 // }
