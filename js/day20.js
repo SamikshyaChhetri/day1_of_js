53a56f67c4330104692ae07d50d3e7e052ad1fdf
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-05-22 22:22:29 +0545

    initial commit for local storage

---
+++ js/day20.js
@@ -0,0 +1,21 @@
+// localStorage.setItem("lastname", "Chhetri");
+// localStorage.setItem("firstname", "Samikshya");
+// document.getElementById("demo").innerHTML = localStorage.getItem("firstname");
+document.getElementById("demo");
+localStorage.setItem(
+  {
+    id: "1",
+    title: "task 1",
+    description: "Description 1",
+  },
+  {
+    id: "2",
+    title: "task 2",
+    description: "Description 2",
+  },
+  {
+    id: "3",
+    title: "task 3",
+    description: "Description 3",
+  }
+);
